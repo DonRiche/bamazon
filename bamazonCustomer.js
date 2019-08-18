f5f659b82e506af4e7681f42d60ca9d3a23dc14b
Ricardo Perdomo <hndgd1980@gmail.com>
2019-08-18 17:04:17 -0500

    item search working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 //var databasePassword = require(".env");
-var productsQuery = "SELECT * FROM products";
-
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -13,22 +11,47 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "",
+    password: "Audrill@9",
     database: "bamazon_db"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     queryAllProducts();
-    //runSearch();
 });
+console.log("----------------BAMAZON-------------------");
 
 function queryAllProducts() {
+    var productsQuery = "SELECT * FROM products";
     connection.query(productsQuery, function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].items_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + "|" + res[i].stock_quantity);
         }
-        console.log("-----------------------------------");
+        console.log("----------------BAMAZON-------------------");
+        itemSearch();
+
     });
+}
+
+function itemSearch() {
+    inquirer
+        .prompt({
+            name: "itemId",
+            type: "input",
+            message: "What is the ID of the product you would like to buy? \n",
+            //message: "How many units of the product you would like to buy?"
+
+        })
+        .then(function (answer) {
+            var itemQuery = "SELECT items_id,product_name,price FROM products WHERE ?";
+            connection.query(itemQuery, { items_id: answer.itemId }, function (err, res) {
+                for (var i = 0; i < res.length; i++) {
+                    console.log("----------------BAMAZON-------------------");
+
+                    console.log("Buying:" + res[i].product_name + " at $" + res[i].price);
+                }
+                //runSearch();
+            });
+        });
 }
