9e69c37e852ac88df04996062e36ebbc0fa65634
Ricardo Perdomo <hndgd1980@gmail.com>
2019-09-29 14:02:12 -0500

    bamazonCustomer.js printing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,3 +32,66 @@ var showTable = function () {
     })
 }
 
+
+var askUser = function (res) {
+
+
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "What would you like to buy? (to exit press Q)"
+
+
+    }]).then(function (answer) {
+
+        var correct = false;
+
+
+        if (answer.choice.toUpperCase() == "Q") {
+            process.exit();
+        }
+
+        for (var i = 0; i < res.length; i++) {
+
+
+            if (res[i].product_name == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+
+
+                inquirer.prompt({
+                    type: "input",
+                    name: "quantity",
+                    message: "How many would you like to buy?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+
+                    //Update the database
+                }).then(function (answer) {
+                    if ((res[id].stock_quantity - answer.quantity) > 0) {
+                        connection.query("UPDATE products SET stock_quantity=' " + (res[id].stock_quantity - answer.quantity) + " ' WHERE product_name='" + product + "'", function (err, res2) {
+                            console.log("Prouduct is Purchased!");
+                            showTable();
+                        })
+
+                    } else {
+                        console.log("Insufficient quantity!");
+                        promptUser(res);
+                    }
+                })
+            }
+        }
+
+        // If wrong value
+        if (i == res.length && correct == false) {
+            console.log("Not a valid selection!");
+            promptUser(res);
+        }
+    })
+}
